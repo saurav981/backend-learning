5734f90afe557ee3e5cdce1b09e12a473c33fa2c
saurav981 <souravhalder981@gmail.com>
2024-09-03 19:41:21 +0530

    Add envsample to gitignore

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+dotenv.config({ path: "./env" });
+
+connectDB()
+  .then(() => {
+    app.on("error", (err) => console.error("Error occured. Reason: ", err));
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log("MondoDB connection failed !!! 😥"));
 
 /*
 import express from "express";
